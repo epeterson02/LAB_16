c7729987fa6d916a6e6565dec64525f4fa8f5ad8
epeterson02 <157244412+epeterson02@users.noreply.github.com>
2025-03-25 15:29:03 -0500

    Python Script + HTML code

---
+++ Lab 16/WebLEDS.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from gpiozero import LED
+
+app = Flask(__name__)
+led = LED(18) # Change to match your LED!
+
+@app.route('/')
+def index():
+    return render_template('HTML Code.html')
+
+@app.route('/toggle/<action>')
+def LEDControl(action):
+    if action == 'on':
+        led.on()
+        return 'LED Turned On'
+    elif action == 'off':
+        led.off()
+        return 'LED Turned Off'
+if __name__ == '__main__':
+    app.run(host='127.0.1.1', port=8080)
